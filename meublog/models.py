aa7d1a625fc234c72f86fb9dfb01ea6b5caed469
Anderson <anderson_silva05@hotmail.com>
2021-12-02 21:47:25 -0300

    Initial commit

---
+++ meublog/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('rascunho', 'Rascunho'),
+        ('publicado', 'Publicado')
+    )
+
+
+titulo = models.CharField(max_length=250)
+slug = models.SlugField(max_length=100)
+corpo = models.TextField()
+autor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='meublog_posts')
+publicado = models.DateTimeField(default=timezone.now)
+criado = models.DateTimeField(auto_now_add=True)
+status = models.CharField(max_length=9, choices='STATUS_CHOICES', default='rascunho')
+
+
+class Meta:
+    ordering = ('-publicado',)
+    verbose_name = "Post"
+    verbose_name_plural = "Posts"
+
+
+def __str__(self):
+    return self.titulo
